92e8643b26d84be702edb0e60ceb1f04b41c7bf6
Weyler03 <wecdr03@gmail.com>
2025-02-25 08:59:05 -0400

    Adding something to package json and viteConfig

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https//weyler03.github.io/Facturacion-Camille',
+  base: "/Facturacion-Camille",
 })
